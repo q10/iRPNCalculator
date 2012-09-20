fe3a512a399923af3067f13051f3ff56833b58a4
Benson <benson@loaner1.dhcp.lbl.gov>
2012-09-19 17:56:58 -0700

    new files

---
+++ Calculator/Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Benson on 9/19/12.
+//  Copyright (c) 2012 q10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Benson on 9/19/12.
+//  Copyright (c) 2012 q10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
